2c1ffcc2f5bd6dcf22fc961e16872f2b5fe473bd
kongo <d.parshenko@gmail.com>
2023-11-06 06:42:02 +0200

    Outline dependecies between models

--- app/models/list_section.rb
+++ app/models/list_section.rb
@@ -1,4 +1,4 @@
 class ListSection < ApplicationRecord
   belongs_to :todo_list
-  has_many :tasks
+  has_many :tasks, dependent: :destroy
 end
--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -1,5 +1,5 @@
 class TodoList < ApplicationRecord
-  has_many :list_sections
+  has_many :list_sections, dependent: :destroy
   validates :title, presence: true
 
   broadcasts_to ->(todo_list) { "todo_lists" }, inserts_by: :prepend
