f11202e71b129c51fc60c606bd359a94a2f0b918
kongo <d.parshenko@gmail.com>
2023-11-07 19:22:36 +0200

    Turbo stream for list sections

--- app/models/list_section.rb
+++ app/models/list_section.rb
@@ -2,4 +2,26 @@ class ListSection < ApplicationRecord
   belongs_to :todo_list
   has_many :tasks, dependent: :destroy
   validates :title, presence: true
+
+  after_create_commit ->(list_section) {
+    broadcast_prepend_to "list_sections", target: list_section.list_dom_id, partial: "list_sections/list_section", locals: { list_section: list_section }
+  }
+
+  after_update_commit ->(list_section) {
+    broadcast_replace_to "list_sections", target: list_section.dom_id, partial: "list_sections/list_section", locals: { list_section: list_section }
+  }
+
+  after_destroy_commit ->(list_section) {
+    broadcast_remove_to "list_sections", target: list_section.dom_id
+  }
+
+  protected
+
+  def dom_id
+    "todo_list_#{todo_list.id}_list_section_#{id}"
+  end
+  
+  def list_dom_id
+    "todo_list_#{todo_list.id}_list_sections"
+  end
 end
